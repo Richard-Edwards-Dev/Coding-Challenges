58af14cdce9a973baf8089ee86c6f8c4988ee284
Rkened <edwardsrichardk@gmail.com>
2022-12-12 11:49:13 -0800

    7kyu strings

---
+++ 7kyu/removeAnchor.js
@@ -0,0 +1,6 @@
+//Avoiding regex to focus on fundamentals
+function removeUrlAnchor(url){
+    return url.split('#')[0]
+}
+
+//https://www.codewars.com/kata/51f2b4448cadf20ed0000386/train/javascript
