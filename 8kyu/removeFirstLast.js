351f7b3807cfac189f62470db48575d69d7d68f6
Rkened <edwardsrichardk@gmail.com>
2022-12-21 18:43:37 -0800

    More 8kyu fundamentals

---
+++ 8kyu/removeFirstLast.js
@@ -0,0 +1,8 @@
+function removeChar(str){
+    return str.substring(1,str.length-1)
+};
+   
+   
+//   https://www.codewars.com/kata/56bc28ad5bdaeb48760009b0/train/javascript
+
+//could have used str.slice(1,-1)
