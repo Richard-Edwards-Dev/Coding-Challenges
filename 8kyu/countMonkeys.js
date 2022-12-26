2ac8a6273a983242444a57818be7ff18e47479d3
Rkened <edwardsrichardk@gmail.com>
2022-12-26 11:29:44 -0800

    Array fundamentals

---
+++ 8kyu/countMonkeys.js
@@ -0,0 +1,9 @@
+function monkeyCount(n) {
+    let count = []
+    for(let i = 1; i <= n; i++){
+      count.push(i)
+    }
+     return count
+   }
+
+//https://www.codewars.com/kata/56f69d9f9400f508fb000ba7/train/javascript
