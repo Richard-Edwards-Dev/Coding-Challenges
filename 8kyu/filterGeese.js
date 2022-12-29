65ba8f6ec81e631f9321ca761a6ab62eef6f791c
Rkened <edwardsrichardk@gmail.com>
2022-12-28 19:33:03 -0800

    Array Fundamentals

---
+++ 8kyu/filterGeese.js
@@ -0,0 +1,6 @@
+function gooseFilter (birds) {
+    var geese = ["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"];
+    return birds.filter((x)=> !geese.includes(x))
+  };
+
+  //https://www.codewars.com/kata/57ee4a67108d3fd9eb0000e7/train/javascript
