791687c220c5be529479cd21a128f1c8de06cfb1
Rkened <edwardsrichardk@gmail.com>
2022-12-31 17:05:09 -0800

    Minor Correction

--- 7kyu/removeMinimum.js
+++ 7kyu/removeMinimum.js
@@ -7,7 +7,7 @@ function removeSmallest(numbers) {
 
   //https://www.codewars.com/kata/563cf89eb4747c5fb100001b/train/javascript
   //Not permitted to affect the original array. Could have found the index of the smallest number and filtered the original
-  //array for every element but the smalles based on index
+  //array for every element but the smallest based on index
 
   // let least = numbers.indexOf(Math.min(...numbers))
   // return numbers.filter((element,index) => index !=== least)
