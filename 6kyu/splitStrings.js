96839ba420b0722e1328a5d36066d7b0cc5c656a
Richard-Edwards-Dev <edwardsrichardk@gmail.com>
2023-02-09 08:29:45 -0800

    6kyu strings

---
+++ 6kyu/splitStrings.js
@@ -0,0 +1,12 @@
+function solution(str){
+    let result = []
+    if(str.length % 2 !== 0){
+      str += '_'
+    }
+    for(let i = 0; i < str.length; i+=2){
+      result.push(str.slice(i,i+2))
+    }
+   return result
+ }
+ 
+ //https://www.codewars.com/kata/515de9ae9dcfc28eb6000001/train/javascript
