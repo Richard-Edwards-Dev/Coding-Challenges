1b75ac6ae735b849e3434b1c6382ed4479847c6d
Richard-Edwards-Dev <edwardsrichardk@gmail.com>
2023-02-26 15:02:22 -0800

    5kyu strings

--- 5Kyu/scrambleLargeStrings.js
+++ 5Kyu/scrambleLargeStrings.js
@@ -6,17 +6,19 @@ function scramble(str1, str2) {
      if(!str1.includes(letter)){
        return false }
      else{
-        hash1[letter] ? hash1[letter]++ : hash1[letter] = 1
+        hash2[letter] ? hash2[letter]++ : hash2[letter] = 1
     }
       }
     for(let j = 0; j< str1.length; j++){
       let letter = str1.charAt(j)
-      hash2[letter] ? hash2[letter]++ : hash2[letter] = 1
+      hash1[letter] ? hash1[letter]++ : hash1[letter] = 1
     }
-    for(let letter in hash1){
-      if(hash2[letter] < hash1[letter]){
+    for(let letter in hash2){
+      if(hash1[letter] < hash2[letter]){
         return false
       }
     }
     return true
   }
+
+//https://www.codewars.com/kata/55c04b4cc56a697bb0000048/train/javascript
