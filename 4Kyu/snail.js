f743876ad5bdbfb01cc55912ab2f69efbf9285f3
Richard-Edwards-Dev <edwardsrichardk@gmail.com>
2023-03-03 09:36:03 -0800

    4kyu arrays

---
+++ 4Kyu/snail.js
@@ -0,0 +1,22 @@
+snail = function(array) {
+    let result = []
+    if (array.length === 1) return array[0]
+    while(array.length > 0){
+      result.push(array.shift())
+      if(array.length > 0){
+      array.forEach(a => result.push(a.pop()))
+        }
+      if(array.length > 0){
+      let last = array.pop()
+      result.push(last.reverse())
+      if(array.length > 0){
+        let temp = []
+        array.forEach(a => temp.push(a.shift()))
+        result.push(temp.reverse())
+      }
+  }
+    }
+    return result.flat()
+  }
+
+//https://www.codewars.com/kata/521c2db8ddc89b9b7a0000c1/train/javascript
