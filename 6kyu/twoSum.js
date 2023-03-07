000517a25c5fcd94fff84fd85c9fdf2fa9a4457b
Richard-Edwards-Dev <edwardsrichardk@gmail.com>
2023-03-06 21:03:17 -0800

    6kyu arrays

---
+++ 6kyu/twoSum.js
@@ -0,0 +1,9 @@
+function twoSum(numbers, target) {
+    for(let i = 0; i < numbers.length; i++){
+      for(let j = i + 1; j < numbers.length; j++){
+        if(numbers[i] + numbers[j] === target) return [i,j]
+      }
+    }
+  }
+
+//https://www.codewars.com/kata/52c31f8e6605bcc646000082/solutions/javascript
